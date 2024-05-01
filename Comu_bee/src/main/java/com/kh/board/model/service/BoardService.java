32ffced248aff3116d43acda2f37296bc7567c2f
CNCDochi <shyu9922@gmail.com>
2024-05-01 16:06:24 +0900

    2.

--- Comu_bee/src/main/java/com/kh/board/model/service/BoardService.java
+++ Comu_bee/src/main/java/com/kh/board/model/service/BoardService.java
@@ -1,5 +1,20 @@
 package com.kh.board.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.board.model.vo.Board;
+import com.kh.common.model.vo.PageInfo;
+
 public class BoardService {
+
+	public int listCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public ArrayList<Board> selectList(PageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
