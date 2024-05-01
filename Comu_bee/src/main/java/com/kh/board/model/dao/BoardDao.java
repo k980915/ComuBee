dba970d90e88084746ca84b43df6e4b2589bc769
CNCDochi <shyu9922@gmail.com>
2024-05-01 16:07:11 +0900

    3.

--- Comu_bee/src/main/java/com/kh/board/model/dao/BoardDao.java
+++ Comu_bee/src/main/java/com/kh/board/model/dao/BoardDao.java
@@ -1,5 +1,87 @@
 package com.kh.board.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import com.kh.board.model.vo.Board;
+import com.kh.common.JDBCTemplate;
+import com.kh.common.model.vo.PageInfo;
+
 public class BoardDao {
+	private Properties prop = new Properties();
+	public BoardDao() {
+		String filepath = BoardDao.class.getResource("/resources/sql/board-mapper.xml").getPath();
+			try {
+				prop.loadFromXML(new FileInputStream(filepath));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+	public ArrayList<Board> selectList(Connection conn, PageInfo pi) {
+		// TODO Auto-generated method stub
+		ArrayList<Board> list = new ArrayList<>();
+		Statement stmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectList");
+		try {
+			stmt=conn.createStatement();
+			rset=stmt.executeQuery(sql);
+			
+			while(rset.next()) {
+				list.add(new Board(
+						rset.getInt("BOARDNO"),
+						rset.getString("CATEGORY"),
+						rset.getString("TITLE"),
+						rset.getString("USERID"),
+						rset.getDate("CREATEDATE"),
+						rset.getInt("BOARDLIKE"),
+						rset.getInt("COUNT")
+						));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(stmt);
+		}
+		
+		return list;
+	}
+
+	public int listCount(Connection conn) {
+		// TODO Auto-generated method stub
+		int result=0;
+		Statement stmt=null;
+		ResultSet rset=null;
+		String sql=prop.getProperty("listCount");
+		
+		try {
+			stmt=conn.createStatement();
+			rset=stmt.executeQuery(sql);
+			if(rset.next()) {
+				result=rset.getInt("COUNT");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(stmt);
+		}
+		
+		
+		return result;
+	}
 
 }
